a572199414ec98b2e11b72ea38883de8f145ad3c
silviamarugan <silvia.mb2912@gmail.com>
2021-09-21 13:07:53 +0200

    Update: Ejercicio 2

--- Miniejercicios_Marugan_Silvia/Assets/Scripts/Ejercicio_2.cs
+++ Miniejercicios_Marugan_Silvia/Assets/Scripts/Ejercicio_2.cs
@@ -6,23 +6,59 @@ public class Ejercicio_2 : MonoBehaviour
 {
 
 
+    int random;
+    int num;
   
-    int random = 0;
-    [SerializeField] int numElegido = 57;
+    [SerializeField] int numElegido = 7;
 
     // Start is called before the first frame update
     void Start()
     {
+        //bucle con while
+        //num = 0;
 
-        while (random != numElegido) 
+       // while (random != numElegido)
         {
-            random = Random.Range(0, 100);
-            print(random);
+          //  random = Random.Range(0, 15);
+         //   print(random);
+        }
+
+        { 
+
+         //fin bucle while
+
+
+
+
+        //bucle con for
+        for (int random2 = 0; random2 != numElegido; random2 = Random.Range(1,15))
+            {
+                print("Aleatorio FOR es igual a " + random2);
+                if (random2 < 5)
+                {
+                    print("Aleatorio FOR es menor que 5");
+
+
+                }
+                else if (random2 > 5)
+                {
+
+                    print("Aleatorio FOR es mayor que 5");
+                }
+
+
+
+            }
+        //fin bucle for
         }
       
-        
 
-        }
+
+
+
+
+
+    }
 
     // Update is called once per frame
     void Update()
