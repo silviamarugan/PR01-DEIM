969ba934aaa00f15ecb8a14da75da6dfad46fd3b
silviamarugan <silvia.mb2912@gmail.com>
2021-09-21 09:48:39 +0200

    Ejercicio 1

--- Miniejercicios_Marugan_Silvia/Assets/Scripts/Ejercicio_1.cs
+++ Miniejercicios_Marugan_Silvia/Assets/Scripts/Ejercicio_1.cs
@@ -2,12 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Ejercicio_1 : MonoBehaviour
+
+   
 {
+    [SerializeField] float num1 = 2.5f;
+    [SerializeField] int num2 = 1;
+
+    public string nombre = " Silvia ";
+    bool correcto;
+    private string mensaje;
+    private float resultado;
+  
     // Start is called before the first frame update
     void Start()
     {
         
+
+        mensaje = ("Hola" + nombre + ":)" );
+        resultado = (num1 * num2);
+        print(resultado);
+        print(mensaje);
     }
 
     // Update is called once per frame
@@ -15,4 +30,5 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
+  
 }
